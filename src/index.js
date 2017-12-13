6dd539a0b7bdee569d051c6247ee18d47cd5591a
Ivan Tyshchenko <TihonV@users.noreply.github.com>
2017-12-13 08:34:51 +0000

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = (
+  <h2>Hello world</h2>
+);
+
+ReactDOM.render(App, document.getElementById('root'));
