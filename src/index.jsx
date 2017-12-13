780ee2dc878141bbe101c26abfcf9d850357ecae
Ivan Tyshchenko <TihonV@users.noreply.github.com>
2017-12-13 08:34:39 +0000

    Delete index.jsx

--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-const App = (
-  <h2>Hello world</h2>
-);
-
-ReactDOM.render(App, document.getElementById('root'));
